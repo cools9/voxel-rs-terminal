e013ba2ae9d9f8e2fad078036e13a464f3e8ecc3
Cools9 <rishabh4a@christacademy.in>
2026-06-20 15:51:38 +0530

    experimenting with how i can possibly project it?

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,41 @@
-use std::io::{self, Write};
-use crossterm::{
-    execute, queue,
-    style::{self, Stylize}, cursor, terminal
-};
+/*
+const SIZE: usize = 16;
 
-fn main() -> io::Result<()> {
-  let mut stdout = io::stdout();
+struct World {
+    voxels: [[[bool; SIZE]; SIZE]; SIZE],
+}
+*/
 
-  execute!(stdout, terminal::Clear(terminal::ClearType::All))?;
+struct Player {
+    x: f32,
+    y: f32,
+    z: f32,
+    yaw: f32,
+    pitch: f32,
+}
 
-  for y in 0..40 {
-    for x in 0..150 {
-      if (y == 0 || y == 40 - 1) || (x == 0 || x == 150 - 1) {
-        // in this loop we are more efficient by not flushing the buffer.
-        queue!(stdout, cursor::MoveTo(x,y), style::PrintStyledContent( "█".magenta()))?;
-      }
-    }
-  }
-  stdout.flush()?;
-  Ok(())
+struct Block{
+    x:f32,
+    y:f32,
+    z:f32
+}
+
+fn main(){
+  let mut player= Player{
+    x:0.0,
+    y:0.0,
+    z:0.0,
+    yaw:0.0,
+    pitch:0.0
+  };
+  let mut block=Block { x:0.0,
+     y:0.0,
+      z:5.0 };
+
+    let dx=block.x-player.x;
+    let dy= block.y-player.y;
+    let dz= block.z-player.z;
+    let distance=(dx * dx + dy * dy + dz * dz).sqrt();
+    let size = (20.0 / distance) as u16;
+    println!("{}",size);
 }
