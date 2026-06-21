f8bb0206093cd67e649309c42f6859a371ca2ce4
Cools9 <rishabh4a@christacademy.in>
2026-06-21 16:36:15 +0530

    added a single cube with a grid

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,22 @@
-/*
-const SIZE: usize = 16;
+use raylib::prelude::*;
 
-struct World {
-    voxels: [[[bool; SIZE]; SIZE]; SIZE],
-}
-*/
-use crossterm::{
-    ExecutableCommand, QueueableCommand, cursor,
-    style::{self, Stylize},
-    terminal,
-};
-use std::io::{self, Write};
-
-struct Player {
-    x: f32,
-    y: f32,
-    z: f32,
-    yaw: f32,
-    pitch: f32,
-}
-
-struct Block {
-    x: f32,
-    y: f32,
-    z: f32,
-}
-
-fn main() -> io::Result<()>{
-    let mut player = Player {
-        x: 0.0,
-        y: 0.0,
-        z: 0.0,
-        yaw: 0.0,
-        pitch: 0.0,
-    };
-    let mut block = Block {
-        x: 0.0,
-        y: 0.0,
-        z: 5.0,
-    };
-
-    let dx = block.x - player.x;
-    let dy = block.y - player.y;
-    let dz = block.z - player.z;
-    let distance = (dx * dx + dy * dy + dz * dz).sqrt();
-    let size = (20.0 / distance) as u16;
-    println!("{}", size);
-    let mut stdout = io::stdout();
-
-    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
-
-    for y in 0..size {
-        for x in 0..size{
-            stdout
-                .queue(cursor::MoveTo(x,y))?
-                .queue(style::PrintStyledContent( "█".magenta()))?;
-            
+fn main(){
+    let (mut rl, thread) = raylib::init().size(800, 600).title("demo").build();
+    let camera = Camera3D::perspective(
+        Vector3::new(4.0, 4.0, 4.0),
+        Vector3::new(0.0, 0.0, 0.0),
+        Vector3::new(0.0, 1.0, 0.0),
+        45.0,
+    );
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::RAYWHITE);
+        {
+            let mut c = d.begin_mode3D(camera);
+            c.draw_cube(Vector3::zero(), 2.0, 2.0, 2.0, Color::RED);
+            c.draw_grid(10, 1.0);
+            // Drop ends 3D mode automatically.
         }
+        d.draw_text("hello 3D", 10, 10, 20, Color::BLACK);
     }
-    stdout.flush()?;
-    Ok(())
 }
