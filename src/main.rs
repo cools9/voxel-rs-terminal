7f718984d16708317ea751b91aab0234707b1900
Cools9 <rishabh4a@christacademy.in>
2026-06-22 16:00:07 +0530

    added a 128x128 platform

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use raylib::prelude::*;
 use raylib::consts::CameraMode;
+
+
+
 fn main(){
     let (mut rl, thread) = raylib::init()
     .size(1280, 720)
@@ -8,7 +11,7 @@ fn main(){
     .vsync()
     .msaa_4x()
     .build();
-    
+    let world_size:u64=128;
     let mut camera = Camera3D::perspective(
         Vector3::new(4.0, 4.0, 4.0),
         Vector3::new(0.0, 0.0, 0.0),
@@ -24,8 +27,12 @@ fn main(){
         d.hide_cursor();
         {
             let mut c = d.begin_mode3D(camera);
-            c.draw_cube(Vector3::zero(), 2.0, 2.0, 2.0, Color::RED);
-            c.draw_grid(10, 1.0);
+            for x in 0..world_size{
+                for z in 0..world_size{
+                    c.draw_cube(Vector3::new(x as f32,0.0,z as f32), 1.0, 1.0, 1.0, Color::RED);
+                }
+            }
+            //c.draw_grid(10, 1.0);
             // Drop ends 3D mode automatically.
         }
     }
