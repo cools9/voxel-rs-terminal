f9faece3d4b59fa8264d184167f269e4b363d793
Cools9 <rishabh4a@christacademy.in>
2026-07-11 20:20:19 +0530

    i added FREE CAMERA again because i was only seeing one voxel face fsr

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ fn main() {
         .fullscreen()
         .vsync()
         .build();
-    let world_size_chunks: i64 = 4;
+    let world_size_chunks: i64 = 8;
+    /*
     let mut player = world::Player { x: 0, y: 0, z: 0 };
     let mut camera = Camera3D::perspective(
         Vector3::new(player.x as f32, player.y as f32, player.z as f32),
@@ -19,14 +20,21 @@ fn main() {
         Vector3::new(0.0, 1.0, 0.0),
         45.0,
     );
-
+    */
+    let mut camera = Camera3D::perspective(
+        Vector3::new(50.0, 80.0, -100.0),
+        Vector3::new(32.0, 30.0, 32.0),
+        Vector3::new(0.0, 1.0, 0.0),
+        45.0,
+    );
     let world = world::generate_world(world_size_chunks);
     let models = world::build_world_meshes(&world, &thread, &mut rl);
 
     let mut direction = movement::NONE;
     while !rl.window_should_close() {
-        rl.disable_cursor();
-
+        rl.hide_cursor();
+        camera.update_camera(CameraMode::CAMERA_FREE);
+        /*
         direction = if rl.is_key_down(KeyboardKey::KEY_W) {
             movement::FRONT
         } else if rl.is_key_down(KeyboardKey::KEY_S) {
@@ -52,8 +60,9 @@ fn main() {
             movement::UP => player.y += 5,
             movement::DOWN => player.y -= 5,
         }
-        camera.position = Vector3::new(player.x as f32, player.y as f32, player.z as f32);
-        camera.target = Vector3::new(player.x as f32, player.y as f32, player.z as f32 + 1.0);
+        */
+        //camera.position = Vector3::new(player.x as f32, player.y as f32, player.z as f32);
+        //camera.target = Vector3::new(player.x as f32, player.y as f32, player.z as f32 + 1.0);
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::SKYBLUE);
         d.draw_fps(0, 0);
